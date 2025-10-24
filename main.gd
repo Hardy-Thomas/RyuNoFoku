ddb0705850f95409be997aa7625741be09bdd3d4
Thomas-Hardy <thomardy92@gmail.com>
2025-10-24 12:57:32 +0200

    add ingredient

--- main.gd
+++ main.gd
@@ -1,5 +1,21 @@
 extends Node2D
 
+const INGREDIENTS = [
+	{"name": "ChickenBroth", "picture": "res://picture/ingredient/ChickenBroth.png"},
+	{"name": "VegetableBroth", "picture": "res://picture/ingredient/VegetableBroth.png"},
+	{"name": "PorkBroth", "picture": "res://picture/ingredient/PorkBroth.png"},
+	{"name": "Tare", "picture": "res://picture/ingredient/Tare.png"},
+	{"name": "ScentedOil", "picture": "res://picture/ingredient/ScentedOil.png"},
+	{"name": "PickledEggs", "picture": "res://picture/ingredient/PickledEggs.png"},
+	{"name": "Menma", "picture": "res://picture/ingredient/Menma.png"},
+	{"name": "Chashu", "picture": "res://picture/ingredient/Chashu.png"},
+	{"name": "RiceNoodles ", "picture": "res://picture/ingredient/RiceNoodles.png"},
+	{"name": "WheatNoodles ", "picture": "res://picture/ingredient/WheatNoodles.png"},
+	{"name": "SpringOnions", "picture": "res://picture/ingredient/SpringOnions.png"},
+	{"name": "NoriSheets", "picture": "res://picture/ingredient/NoriSheets.png"},
+	{"name": "Narutomaki", "picture": "res://picture/ingredient/Narutomaki.png"}
+]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
