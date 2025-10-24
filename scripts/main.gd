722df34a40b8789cc60740946f7a2cda3afea5e5
Thomas-Hardy <thomardy92@gmail.com>
2025-10-24 22:48:14 +0200

    add category

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,53 +1,102 @@
 extends Node2D
 
 const INGREDIENTS = [
-	{"name": "ChickenBroth", "picture": "res://picture/ingredient/ChickenBroth.png"},
-	{"name": "VegetableBroth", "picture": "res://picture/ingredient/VegetableBroth.png"},
-	{"name": "PorkBroth", "picture": "res://picture/ingredient/PorkBroth.png"},
-	{"name": "Tare", "picture": "res://picture/ingredient/Tare.png"},
-	{"name": "ScentedOil", "picture": "res://picture/ingredient/ScentedOil.png"},
-	{"name": "PickledEggs", "picture": "res://picture/ingredient/PickledEggs.png"},
-	{"name": "Menma", "picture": "res://picture/ingredient/Menma.png"},
-	{"name": "Chashu", "picture": "res://picture/ingredient/Chashu.png"},
-	{"name": "RiceNoodles ", "picture": "res://picture/ingredient/RiceNoodles.png"},
-	{"name": "WheatNoodles ", "picture": "res://picture/ingredient/WheatNoodles.png"},
-	{"name": "SpringOnions", "picture": "res://picture/ingredient/SpringOnions.png"},
-	{"name": "NoriSheets", "picture": "res://picture/ingredient/NoriSheets.png"},
-	{"name": "Narutomaki", "picture": "res://picture/ingredient/Narutomaki.png"}
+	{"name": "ChickenBroth","category": "Broth", "picture": "res://picture/ingredient/ChickenBroth.png"},
+	{"name": "VegetableBroth","category": "Broth", "picture": "res://picture/ingredient/VegetableBroth.png"},
+	{"name": "PorkBroth","category": "Broth", "picture": "res://picture/ingredient/PorkBroth.png"},
+	{"name": "Tare","category": "Tare", "picture": "res://picture/ingredient/Tare.png"},
+	{"name": "ScentedOil","category": "ScentedOil", "picture": "res://picture/ingredient/ScentedOil.png"},
+	{"name": "PickledEggs","category": "trim", "picture": "res://picture/ingredient/PickledEggs.png"},
+	{"name": "Menma","category": "trim", "picture": "res://picture/ingredient/Menma.png"},
+	{"name": "Chashu","category": "trim", "picture": "res://picture/ingredient/Chashu.png"},
+	{"name": "RiceNoodles ","category": "Noodles", "picture": "res://picture/ingredient/RiceNoodles.png"},
+	{"name": "WheatNoodles ","category": "Noodles", "picture": "res://picture/ingredient/WheatNoodles.png"},
+	{"name": "SpringOnions","category": "condiment", "picture": "res://picture/ingredient/SpringOnions.png"},
+	{"name": "NoriSheets","category": "condiment", "picture": "res://picture/ingredient/NoriSheets.png"},
+	{"name": "Narutomaki","category": "condiment", "picture": "res://picture/ingredient/Narutomaki.png"}
 ]
 
 var secret = ["PorkBroth", "Tare", "ScentedOil", "PickledEggs", "Menma", "Chashu", "WheatNoodles", "SpringOnions", "NoriSheets", "Narutomaki"]
 
 
 # Références UI
+@onready var category_dropdown = $UI/IngredientsShelf/CategoryDropdown
+@onready var ingredient_dropdown = $UI/IngredientsShelf/IngredientDropdown
+@onready var add_button = $UI/IngredientsShelf/AddButton
 @onready var shelf = $UI/IngredientsShelf
 @onready var board = $UI/RecipeBoard
 @onready var feedback = $UI/Feedback
 @onready var validate_button = $UI/Valider
 @onready var phone_feedback = $UI/dialog
+
 var current_guess := []
+var ingredients_by_category := {}
 
 func _ready():
-	setup_shelf()
+	organize_ingredients_by_category()
+	setup_dropdowns()
 	setup_board()
-	
-	
 	position_board_bottom()
+	validate_button.pressed.connect(on_validate)
+	add_button.pressed.connect(on_add_ingredient)
 
+func organize_ingredients_by_category():
+	ingredients_by_category = {}
+	for ingredient in INGREDIENTS:
+		var category = ingredient["category"]
+		if not ingredients_by_category.has(category):
+			ingredients_by_category[category] = []
+		ingredients_by_category[category].append(ingredient)
 
-
-
-
-func setup_shelf():
-	if shelf is HBoxContainer:
-		convert_to_grid_container(shelf)
+func setup_dropdowns():
+	# Setup category dropdown
+	category_dropdown.clear()
+	var categories = ingredients_by_category.keys()
+	categories.sort()
 	
-	for ing in INGREDIENTS:
-		var icon = preload("res://scenes/ingredient_icon.tscn").instantiate()
-		icon.texture = load(ing.picture)
-		icon.ingredient_name = ing.name
-		icon.custom_minimum_size = Vector2(64, 64)
-		shelf.add_child(icon)
+	for category in categories:
+		category_dropdown.add_item(category.capitalize())
+	
+	# Connect signal
+	category_dropdown.item_selected.connect(on_category_selected)
+	
+	# Setup initial ingredient dropdown
+	update_ingredient_dropdown(0)
+
+func on_category_selected(index):
+	update_ingredient_dropdown(index)
+
+func update_ingredient_dropdown(category_index):
+	ingredient_dropdown.clear()
+	
+	var categories = ingredients_by_category.keys()
+	categories.sort()
+	var selected_category = categories[category_index]
+	var ingredients = ingredients_by_category[selected_category]
+	
+	for ingredient in ingredients:
+		ingredient_dropdown.add_item(ingredient["name"])
+
+func on_add_ingredient():
+	var category_index = category_dropdown.selected
+	var ingredient_index = ingredient_dropdown.selected
+	
+	var categories = ingredients_by_category.keys()
+	categories.sort()
+	var selected_category = categories[category_index]
+	var ingredients = ingredients_by_category[selected_category]
+	var selected_ingredient = ingredients[ingredient_index]
+	
+	# Ajouter l'ingrédient à la shelf
+	add_ingredient_to_shelf(selected_ingredient)
+
+func add_ingredient_to_shelf(ingredient):
+	var icon = preload("res://scenes/ingredient_icon.tscn").instantiate()
+	icon.texture = load(ingredient["picture"])
+	icon.ingredient_name = ingredient["name"]
+	icon.custom_minimum_size = Vector2(64, 64)
+	icon.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+	shelf.add_child(icon)
 
 
 func setup_board():
